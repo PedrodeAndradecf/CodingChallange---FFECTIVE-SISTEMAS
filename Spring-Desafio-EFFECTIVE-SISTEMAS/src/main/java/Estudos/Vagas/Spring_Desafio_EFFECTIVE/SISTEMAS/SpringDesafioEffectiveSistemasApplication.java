31b6491464a2a1a23f219909d2cef3fcd45505bb
Pedro Andrade <pedroandrade@222838-MackFCI.local>
2025-08-07 16:51:42 -0300

    Initializer criado

---
+++ Spring-Desafio-EFFECTIVE-SISTEMAS/src/main/java/Estudos/Vagas/Spring_Desafio_EFFECTIVE/SISTEMAS/SpringDesafioEffectiveSistemasApplication.java
@@ -0,0 +1,13 @@
+package Estudos.Vagas.Spring_Desafio_EFFECTIVE.SISTEMAS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDesafioEffectiveSistemasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDesafioEffectiveSistemasApplication.class, args);
+	}
+
+}
---
+++ Spring-Desafio-EFFECTIVE-SISTEMAS/src/test/java/Estudos/Vagas/Spring_Desafio_EFFECTIVE/SISTEMAS/SpringDesafioEffectiveSistemasApplicationTests.java
@@ -0,0 +1,13 @@
+package Estudos.Vagas.Spring_Desafio_EFFECTIVE.SISTEMAS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDesafioEffectiveSistemasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
